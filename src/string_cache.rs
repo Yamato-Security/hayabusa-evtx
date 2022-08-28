f859169b1b36912537658493da8e3fc65fba3548
DastInDark <2350416+hitenkoku@users.noreply.github.com>
2022-08-28 15:00:51 +0900

    To improve speed, changed crate from std::collections::HashMap to hashbrown::HashMap

--- src/string_cache.rs
+++ src/string_cache.rs
@@ -4,7 +4,7 @@ use crate::ChunkOffset;
 
 use log::trace;
 use std::borrow::BorrowMut;
-use std::collections::HashMap;
+use hashbrown::HashMap;
 use std::io::{Cursor, Seek, SeekFrom};
 
 #[derive(Debug)]
